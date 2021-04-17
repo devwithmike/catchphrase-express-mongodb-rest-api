08ab5f768549e586113ea4beeaa4de18bb3bcb34
Mike Meyer <mfmeyer301@gmail.com>
2021-04-17 14:26:29 +0200

    docs

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const docs = require('./docs')
+const catchphrases = require('./catchphrases')
+
+module.exports = {
+	docs,
+	catchphrases
+}
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const connectDb = require("./config/db");
+const { docs, catchphrases } = require("./routes/index");
 
 const app = express();
 connectDb();
 
 app.use(express.json());
 
-const catchphrasesRouter = require('./routes/catchphrases');
-app.use('/catchphrases', catchphrasesRouter)
+app.use(express.static('public'));
+
+app.use('/', docs);
+app.use('/catchphrases', catchphrases)
 
 app.listen(process.env.PORT || 3000, () => console.log('Daar vat hy!!!'));
