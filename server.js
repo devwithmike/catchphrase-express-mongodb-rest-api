b234cc078ec955f2599a201b553dea8be608ec3d
Mike Meyer <mfmeyer301@gmail.com>
2021-05-09 22:26:39 +0200

    removed old docs

--- server.js
+++ server.js
@@ -10,21 +10,19 @@ connectDb();
 
 app.use(express.json());
 
-app.use(express.static('public'));
-
 const swaggerOptions = {
 	swaggerDefinition: {
 		info: {
-			title: 'REST API',
-			description: "REST API INFO"
+			title: 'Catchphrases REST API',
+			description: "A REST API built with Express and MongoDB. This API provides movie catchphrases and the context of the catchphrase in the movie."
 		},
 	},
 	apis: ["./routes/catchphrases.js"]
 }
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/catchphrases', catchphrases)
 
-app.listen(process.env.PORT || 3000, () => console.log('Daar vat hy!!!'));
+app.listen(process.env.PORT || 5000, () => console.log('Daar vat hy!!!'));
